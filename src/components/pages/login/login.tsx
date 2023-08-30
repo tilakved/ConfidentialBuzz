50cf1c9990b0e35cb73e86c71442b81898d7c107
tilakved <tilakved3898@gmail.com>
2023-08-30 15:05:49 +0530

    login UI completion

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -1,20 +1,20 @@
-import {AnimatePresence, motion} from 'framer-motion';
-import Logo from '../../../assets/react.svg';
-import Back from '../../../assets/icons/left-arrow-back-svgrepo-com.svg';
 import './login.scss'
+import {AnimatePresence, motion} from 'framer-motion';
+import Logo from '../../../../public/vite.svg';
+import Back from '../../../assets/icons/left-arrow-back-svgrepo-com.svg';
 import {useState} from "react";
 
 
 function Login() {
     function handleBack() {
-        setshowEmail(true)
+        setShowEmail(true)
     }
 
     function handleContinue() {
-        setshowEmail(false)
+        setShowEmail(false)
     }
 
-    const [showEmail, setshowEmail] = useState(true);
+    const [showEmail, setShowEmail] = useState(true);
     return (
         <>
             <div className="bg-white">
@@ -26,16 +26,15 @@ function Login() {
                             <span className="rounded-full w-12 h-12 flex items-center justify-start content-center">
                                 <img src={Logo} alt=''/>
                             </span>
-                                <a className="font-medium text-2xl titleFont">Confidential Buzz</a>
+                                <a className="font-medium text-3xl titleFont">Confidential Buzz</a>
                             </div>
                             <div className='space-y-5'>
-                                <h1 className="lg:text-3xl xl:text-5xl xl:leading-snug font-extrabold">Enter your
-                                    account and discover new experiences</h1>
+                                <h1 className="lg:text-2xl xl:text-4xl xl:leading-snug font-extrabold">Elevating the Voices of Your Friends with Uncompromised Security on Confidential Buzz.</h1>
                             </div>
                             <p className="font-medium"></p>
                         </div>
 
-                        <div className="flex flex-1 flex-col items-center justify-center px-10 relative">
+                        <div className="flex flex-1 flex-col items-center justify-center px-10 relative bg-chat">
                             <div className="flex lg:hidden justify-between items-center w-full py-4">
                                 <div className="flex items-center justify-start content-center space-x-3">
                             <span className="rounded-full w-12 h-12 flex items-center justify-start content-center">
@@ -44,69 +43,70 @@ function Login() {
                                     <a className="font-medium text-2xl">Confidential Buzz</a>
                                 </div>
                             </div>
-                            <div className="flex flex-1 flex-col justify-center max-w-md">
-                                <div className="flex flex-col">
-                                    {showEmail &&
-                                        <>
-                                            <div >
-                                                <h2 className="text-3xl md:text-4xl font-bold">Sign in to account</h2>
+                            <div className="flex flex-1 flex-col justify-center max-w-md ">
+                                <div className="glass-box">
+                                    <div className="flex flex-col">
+                                        {showEmail &&
+                                            <>
+                                                <div >
+                                                    <h2 className="text-3xl md:text-4xl font-bold">Sign in to account</h2>
+                                                    <p className="text-md md:text-xl">Sign up or log in to place the order, no password
+                                                        require!</p>
+                                                </div>
+                                            </>
+                                        }
+                                        {!showEmail &&
+                                            <div>
+
+                                                <div className="inline-block">
+                                                    <motion.span whileHover={{ scale: 1.05 }}
+                                                                 transition={{ type: "spring", stiffness: 400, damping: 10 }}
+                                                                 className="text-l md:text-xl font-bold flex flex-row gap-1 items-center cursor-pointer"
+                                                                 onClick={() => handleBack()}><span
+                                                        className="rounded-full w-6 h-6 flex items-center justify-start content-center"><i><img
+                                                        alt='' src={Back}/></i></span><h2>Go Back</h2></motion.span>
+                                                </div>
+                                                <h2 className="text-3xl md:text-4xl font-bold">Welcome back !</h2>
                                                 <p className="text-md md:text-xl">Sign up or log in to place the order, no password
                                                     require!</p>
+
                                             </div>
-                                        </>
-                                    }
-                                    {!showEmail &&
-                                        <div>
-
-                                            <div className="inline-block">
-                                                <motion.span whileHover={{ scale: 1.05 }}
-                                                             transition={{ type: "spring", stiffness: 400, damping: 10 }}
-                                                    className="text-l md:text-xl font-bold flex flex-row gap-1 items-center cursor-pointer"
-                                                    onClick={() => handleBack()}><span
-                                                    className="rounded-full w-6 h-6 flex items-center justify-start content-center"><i><img
-                                                    alt='' src={Back}/></i></span><h2>Go Back</h2></motion.span>
-                                            </div>
-                                            <h2 className="text-3xl md:text-4xl font-bold">Welcome back !</h2>
-                                            <p className="text-md md:text-xl">Sign up or log in to place the order, no password
-                                                require!</p>
-
-                                        </div>
-                                    }
-
-
-                                </div>
-                                <div className="flex flex-col max-w-md mt-5 relative">
-                                    <AnimatePresence>
-                                        {showEmail &&
-                                            <motion.input type="text" placeholder="Email"
-                                                          initial={{x: '100%', opacity: 1, position: 'absolute'}}
-                                                          animate={{x: 0, opacity: 1, position: 'initial'}}
-                                                          exit={{x: '100%', opacity: "0", position: "absolute"}}
-                                                          className=" flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
                                         }
-                                    </AnimatePresence>
-                                    <AnimatePresence>
-                                        {!showEmail &&
 
-                                            <motion.input type="text" placeholder="Password"
-                                                          initial={{x: '-100%', opacity: 0, position: 'absolute'}}
-                                                          animate={{x: 0, opacity: 1, position: 'initial'}}
-                                                          className=" flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
 
-                                        }
-                                    </AnimatePresence>
-                                    <button
-                                        className="flex mt-5 items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-black text-white"
-                                        onClick={() => handleContinue()}>
-                                        Continue
-                                    </button>
-                                    <div className="flex justify-center items-center my-5">
-                                        <span className="w-full border border-black"></span>
-                                        <span className="px-4">Or</span>
-                                        <span className="w-full border border-black"></span>
                                     </div>
-                                    <button
-                                        className="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black relative">
+                                    <div className="flex flex-col max-w-md mt-5 relative">
+                                        <AnimatePresence>
+                                            {showEmail &&
+                                                <motion.input type="text" placeholder="Email"
+                                                              initial={{x: '100%', opacity: 1, position: 'absolute'}}
+                                                              animate={{x: 0, opacity: 1, position: 'initial'}}
+                                                              exit={{x: '100%', opacity: "0", position: "absolute"}}
+                                                              className=" flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
+                                            }
+                                        </AnimatePresence>
+                                        <AnimatePresence>
+                                            {!showEmail &&
+
+                                                <motion.input type="text" placeholder="Password"
+                                                              initial={{x: '-100%', opacity: 0, position: 'absolute'}}
+                                                              animate={{x: 0, opacity: 1, position: 'initial'}}
+                                                              className=" flex px-3 py-2 md:px-4 md:py-3 border-2 border-black rounded-lg font-medium placeholder:font-normal"/>
+
+                                            }
+                                        </AnimatePresence>
+                                        <button
+                                            className="flex mt-5 items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black bg-black text-white"
+                                            onClick={() => handleContinue()}>
+                                            Continue
+                                        </button>
+                                        <div className="flex justify-center items-center my-5">
+                                            <span className="w-full border border-black"></span>
+                                            <span className="px-4">Or</span>
+                                            <span className="w-full border border-black"></span>
+                                        </div>
+                                        <button
+                                            className="flex items-center justify-center flex-none px-3 py-2 md:px-4 md:py-3 border-2 rounded-lg font-medium border-black relative">
                                           <span className="absolute left-4">
                                               <svg width="24px" height="24px" viewBox="0 0 24 24"
                                                    xmlns="http://www.w3.org/2000/svg">
@@ -120,8 +120,9 @@ function Login() {
                                                     d="M5.27698177,14.2678769 C5.03832634,13.556323 4.90909091,12.7937589 4.90909091,12 C4.90909091,11.2182781 5.03443647,10.4668121 5.26620003,9.76452941 L1.23999023,6.65002441 C0.43658717,8.26043162 0,10.0753848 0,12 C0,13.9195484 0.444780743,15.7301709 1.23746264,17.3349879 L5.27698177,14.2678769 Z"/>
                                           </svg>
                                           </span>
-                                        <span>Sign in with Google</span>
-                                    </button>
+                                            <span>Sign in with Google</span>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
 
