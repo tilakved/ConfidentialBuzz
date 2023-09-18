09d5d4d8918f362d09767e2aa412c38f7ae68d40
tilakved <tilakved3898@gmail.com>
2023-09-18 15:44:27 +0530

    updated profile page

--- src/components/pages/profile/profile.tsx
+++ src/components/pages/profile/profile.tsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 import {getUserDetails, User} from "../../../API/firebase/database.ts";
 import {successAlert} from "../../../swal/swal.ts";
 import {AiOutlineEdit} from "react-icons/ai";
+import {BsClipboardCheckFill} from "react-icons/bs";
 
 function Profile() {
     const [profileUser, setProfileUser] = useState<User | null>(null)
@@ -61,47 +62,51 @@ function Profile() {
 
     return (profileUser &&
         <div className="w-full h-full p-5">
-            <div className="flex content-center flex-col">
+            <div className="flex content-center flex-col pb-5">
                 <div
                     className="w-full text-9xl text-center font-semibold titleFont uppercase">{profileUser?.displayName}</div>
                 <div className="items-center flex-col content-center flex w-full">
                     <img className="h-64 mt-[-50px] rounded-full text-right" alt='' src={profileUser?.photoURL}/>
                 </div>
-                <code className="flex self-end gap-1.5 items-center">
-                    UID number: <p
-                    className="text-secondary ring-secondary ring-1 rounded-md p-1 bg-secondary/10 leading-none">{profileUser?.uid} </p>
-                    <div
-                        className="rounded-xl dark:text-white dark:border-white w-full sm:w-auto border border-gray-800 text-base font-medium text-gray-800 p-0.5 focus:outline-none hover:bg-gray-800 hover:text-white dark:hover:text-white dark:hover:bg-gray-700 cursor-pointer"
-                        onClick={handleCopyClick}>copy
-                    </div>
-                </code>
+
             </div>
-            <div className='w-1/2 m-auto'>
-                <h1 className="text-2xl font-semibold underline">
-                    Your Details:-
+            <hr className={"w-96 m-auto"}/>
+            <div className='w-1/2 m-auto pt-10'>
+                <h1 className="text-2xl font-semibold uppercase">
+                    Your Details
                 </h1>
-                <div className="flex flex-col gap-3.5 ml-3 mt-3">
-                    {!editMode &&
-                        <>
-                            <span>Your Name :- {profileUser?.displayName}</span>
-                            <span>Your Email :- {profileUser?.email}</span>
-                        </>
-                    }
-                    {editMode &&
-                        <>
-                            <span>Your Name :- <input value={editProfileUser?.displayName}
-                                                      onChange={(event) => updateData('name', event)}
-                                                      placeholder={'name'}
-                                                      className='text-dark rounded-xl p-2'/></span>
-                            <span>Your Email :- <input value={editProfileUser?.email}
-                                                       onChange={(event) => updateData('email', event)}
-                                                       placeholder={'email'}
-                                                       className='text-dark rounded-xl p-2'/> </span>
-                        </>
-                    }
-                    <span
-                        className="flex gap-2 items-center cursor-pointer rounded-xl dark:text-white dark:border-white border border-gray-800 text-base font-medium text-gray-800 focus:outline-none hover:bg-gray-800 hover:text-white dark:hover:text-white dark:hover:bg-gray-700 w-fit p-2"
-                        onClick={() => handleEditMode()}><AiOutlineEdit/>{editMode ? 'Save Changes' : 'Edit Details'}</span>
+                <div className={"pt-5"}>
+                    <code className="flex self-end gap-1.5 items-center">
+                        UID number: <p
+                        className="text-secondary ring-secondary ring-1 rounded-md p-1 bg-secondary/10 leading-none">{profileUser?.uid} </p>
+                        <div
+                            className="rounded-xl dark:text-white dark:border-white w-full sm:w-auto text-base font-medium text-gray-800 p-0.5 focus:outline-none hover:bg-gray-800 hover:text-white dark:hover:text-white dark:hover:bg-gray-700 cursor-pointer"
+                            onClick={handleCopyClick}><BsClipboardCheckFill/>
+                        </div>
+                    </code>
+                    <div className="flex flex-col">
+                        {!editMode &&
+                            <>
+                                <span className={"my-2"}>Your Name :- {profileUser?.displayName}</span>
+                                <span className={"my-2"}>Your Email :- {profileUser?.email}</span>
+                            </>
+                        }
+                        {editMode &&
+                            <>
+                            <span className={"my-2"}><p>Your Name :- </p><input value={editProfileUser?.displayName}
+                                                                            onChange={(event) => updateData('name', event)}
+                                                                            placeholder={'name'}
+                                                                            className='text-dark rounded-xl p-2'/></span>
+                                <span className={"my-2"}><p>Your Name :- </p><input value={editProfileUser?.email}
+                                                                                onChange={(event) => updateData('email', event)}
+                                                                                placeholder={'email'}
+                                                                                className='text-dark rounded-xl p-2'/> </span>
+                            </>
+                        }
+                        <span
+                            className=" text-[12px] flex gap-2 items-center cursor-pointer rounded-xl dark:text-white dark:border-white border border-gray-800 font-medium text-gray-800 focus:outline-none hover:bg-gray-800 hover:text-white dark:hover:text-white dark:hover:bg-gray-700 w-fit p-2 my-2"
+                            onClick={() => handleEditMode()}><AiOutlineEdit/>{editMode ? 'Save Changes' : 'Edit Details'}</span>
+                    </div>
                 </div>
             </div>
         </div>
